afecfb77507fac0141524dff180b7190af5b5e32
awen <zhiwen_der@qq.com>
2023-04-23 19:16:57 +0800

    update

--- examples/mapper_example/StudentMapper.go
+++ examples/mapper_example/StudentMapper.go
@@ -3,5 +3,10 @@ package mapper_example
 import "gitee.com/aurora-engine/gobatis/examples/mapper_example/model"
 
 type StudentMapper struct {
-	AddOne func(student model.Student) error
+	AddOne   func(student model.Student) error
+	InsertId func(student model.Student) (int64, int64, error)
+	Adds     func(ctx any) error
+
+	QueryAll  func() ([]model.Student, error)
+	QueryPage func() ([]model.Student, int64, error)
 }
--- examples/mapper_example/mapper_test.go
+++ examples/mapper_example/mapper_test.go
@@ -2,6 +2,7 @@ package mapper_example
 
 import (
 	"database/sql"
+	"fmt"
 	"gitee.com/aurora-engine/gobatis"
 	"gitee.com/aurora-engine/gobatis/examples/mapper_example/model"
 	_ "github.com/go-sql-driver/mysql"
@@ -22,16 +23,73 @@ func init() {
 	batis.ScanMappers(studentMapper)
 }
 
-func TestInsert(t *testing.T) {
+func TestAddOne(t *testing.T) {
 	s := model.Student{
-		Id:         0,
 		Name:       "test",
 		Age:        1,
 		CreateTime: time.Now().Format("2006-01-02 15:04:05"),
 	}
-	err := studentMapper.AddOne(s)
+
+	if err := studentMapper.AddOne(s); err != nil {
+		t.Error(err.Error())
+		return
+	}
+}
+
+func TestAdds(t *testing.T) {
+	var arr []model.Student
+	for i := 0; i < 10; i++ {
+		s := model.Student{
+			Name:       fmt.Sprintf("test_%d", i),
+			Age:        i + 2,
+			CreateTime: time.Now().Format("2006-01-02 15:04:05"),
+		}
+		arr = append(arr, s)
+	}
+	err := studentMapper.Adds(
+		map[string]any{
+			"arr": arr,
+		},
+	)
 	if err != nil {
 		t.Error(err.Error())
 		return
 	}
+}
+
+func TestInsertId(t *testing.T) {
+	var count, id int64
+	var err error
+	s := model.Student{
+		Name:       "test",
+		Age:        2,
+		CreateTime: time.Now().Format("2006-01-02 15:04:05"),
+	}
+
+	if count, id, err = studentMapper.InsertId(s); err != nil {
+		t.Error(err.Error())
+		return
+	}
+	t.Log("count:", count, "id:", id)
+}
+
+func TestQueryAll(t *testing.T) {
+	var stus []model.Student
+	var err error
+	if stus, err = studentMapper.QueryAll(); err != nil {
+		t.Error(err.Error())
+		return
+	}
+	t.Log(stus)
+}
+
+func TestQueryPage(t *testing.T) {
+	var stus []model.Student
+	var count int64
+	var err error
+	if stus, count, err = studentMapper.QueryPage(); err != nil {
+		t.Error(err.Error())
+		return
+	}
+	t.Log("rows:", stus, "count:", count)
 }
